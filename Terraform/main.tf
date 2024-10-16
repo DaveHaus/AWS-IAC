30170a3f5688fb22a17462d5e1c085d75357052a
DaveHaus <djhausman@gmail.com>
2024-10-15 21:16:35 -0600

    added ansible connection and tf integration

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server" {
+  count         = 2
   ami           = "ami-0fff1b9a61dec8a5f"  # Replace with your desired AMI ID
   instance_type = "t2.micro"   
   key_name      = "A4L"  # Replace with your key pair name
@@ -11,7 +12,7 @@ resource "aws_instance" "web_server" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "WebServer"
+    Name = "WebServer-${count.index}"  # Tag instances uniquely
   }
 }
 
@@ -41,7 +42,23 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-output "public_ip" {
-  value = aws_instance.web_server.public_ip
+# Output the public IP addresses for each instance
+output "public_ips" {
+  value = aws_instance.web_server[*].public_ip
 }
 
+# Output the instance IDs for each instance
+output "instance_ids" {
+  value = aws_instance.web_server[*].id
+}
+
+# Overwrite Ansible inventory file with the new instance ID every time
+resource "local_file" "ansible_inventory" {
+  filename = "/Users/dave/repos/AWS-IAC/Ansible/hosts.yml"  # This file is overwritten on every terraform apply
+  content  = <<-EOF
+[ec2_instances]
+%{ for ip in aws_instance.web_server[*].public_ip }
+${ip}
+%{ endfor }
+  EOF
+}
